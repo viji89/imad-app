e46476bf8c5bdbf3f457fccbcb4ffd32c7d957b6
viji89 <30643739+viji89@users.noreply.github.com>
2017-08-08 11:43:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one', function (req, res){
 });
 
 app.get('/article-two', function (req, res){
-    res.send('Article two is requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function (req, res){
     res.send('Article three is requested');
