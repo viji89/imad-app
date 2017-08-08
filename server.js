6fbba29cca186435a53dcba0cdbd35e72008ad58
viji89 <30643739+viji89@users.noreply.github.com>
2017-08-08 11:55:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 
 app.get('/article-two', function (req, res){
